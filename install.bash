770d0cd2fb6ccff67139bb154ef8823e28be587e
Thomas Ung <tomkimsour@gmail.com>
2024-01-31 16:32:22 +0100

    finish the cooking, setup of top bar and wm

--- install.bash
+++ install.bash
@@ -4,3 +4,4 @@ ln -s $(pwd)/p10k/.p10k.zsh ~/.p10k.zsh
 ln -s $(pwd)/nvim ~/.config/nvim
 ln -s $(pwd)/tmux/tmux.conf ~/.config/tmux/tmux.conf
 ln -s $(pwd)/sketchybar/sketchybarrc ~/.config/sketchybar/sketchybarrc
+ln -s $(pwd)/tmux/plugins/tmux/custom ~/.config/tmux/plugins/custom
