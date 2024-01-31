9e46ab6f27546a6a6ebee652be83d636cbeff552
Thomas Ung <tomkimsour@gmail.com>
2024-01-31 11:09:08 +0100

    add simple script for symbolic link creation and update p10k

---
+++ install.bash
@@ -0,0 +1,6 @@
+# ask if you want to add p10k file
+
+ln -s $(pwd)/p10k/.p10k.zsh ~/.p10k.zsh
+ln -s $(pwd)/nvim ~/.config/nvim
+ln -s $(pwd)/tmux/tmux.conf ~/.config/tmux/tmux.conf
+ln -s $(pwd)/sketchybar/sketchybarrc ~/.config/sketchybar/sketchybarrc
