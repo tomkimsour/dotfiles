444d65432177b6901b51d1c8e1d4742e5d8be837
Thomas Ung <tomkimsour@gmail.com>
2024-05-18 19:00:08 +0200

    update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,48 @@
 return {
   {
+    "folke/neoconf.nvim",
+    opts = {
+
+      -- name of the local settings files
+      local_settings = ".neoconf.json",
+      -- name of the global settings file in your Neovim config directory
+      global_settings = "neoconf.json",
+      -- import existing settings from other plugins
+      import = {
+        vscode = true, -- local .vscode/settings.json
+        coc = true, -- global/local coc-settings.json
+        nlsp = true, -- global/local nlsp-settings.nvim json settings
+      },
+      -- send new configuration to lsp clients when changing json settings
+      live_reload = true,
+      -- set the filetype to jsonc for settings files, so you can use comments
+      -- make sure you have the jsonc treesitter parser installed!
+      filetype_jsonc = true,
+      plugins = {
+        -- configures lsp clients with settings in the following order:
+        -- - lua settings passed in lspconfig setup
+        -- - global json settings
+        -- - local json settings
+        lspconfig = {
+          enabled = true,
+        },
+        -- configures jsonls to get completion in .nvim.settings.json files
+        jsonls = {
+          enabled = true,
+          -- only show completion in json settings for configured lsp servers
+          configured_servers_only = true,
+        },
+        -- configures lua_ls to get completion of lspconfig server settings
+        lua_ls = {
+          -- by default, lua_ls annotations are only enabled in your neovim config directory
+          enabled_for_neovim_config = true,
+          -- explicitely enable adding annotations. Mostly relevant to put in your local .nvim.settings.json file
+          enabled = false,
+        },
+      },
+    },
+  },
+  {
     "williamboman/mason.nvim",
     cmd = "Mason",
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
@@ -27,6 +70,50 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      -- options for vim.diagnostic.config()
+      ---@type vim.diagnostic.Opts
+      diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = {
+          spacing = 4,
+          source = "if_many",
+          prefix = "●",
+          -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+          -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+          -- prefix = "icons",
+        },
+        severity_sort = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+            [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+            [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+            [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+          },
+        },
+      },
+      -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+      -- Be aware that you also will need to properly configure your LSP server to
+      -- provide the inlay hints.
+      inlay_hints = {
+        enabled = true,
+      },
+      -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+      -- Be aware that you also will need to properly configure your LSP server to
+      -- provide the code lenses.
+      codelens = {
+        enabled = false,
+      },
+      -- add any global capabilities here
+      capabilities = {},
+      -- options for vim.lsp.buf.format
+      -- `bufnr` and `filter` is handled by the LazyVim formatter,
+      -- but can be also overridden when specified
+      format = {
+        formatting_options = nil,
+        timeout_ms = nil,
+      },
       servers = {
         -- ruff = {
         --   -- on_attach = function(client, bufnr)
