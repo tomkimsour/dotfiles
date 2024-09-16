edae1ffc285f962a509e0501ebfb4540d25d7517
thomasung <thomas.ung@pal-robotics.com>
2024-09-16 16:41:10 +0200

    add just and remove f1 keybind

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,9 +8,20 @@ return {
       "markdown",
       "markdown_inline",
     })
+    opts.parsers.get_parser_configs().just = {
+      install_info = {
+        url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
+        files = { "src/parser.c", "src/scanner.c" },
+        branch = "main",
+        -- use_makefile = true -- this may be necessary on MacOS (try if you see compiler errors)
+      },
+    }
   end,
   ensure_installed = { "markdown", "markdown_inline" },
   highlight = {
     enable = true,
+  },
+  {
+    "IndianBoy42/tree-sitter-just",
   },
 }
