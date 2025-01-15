6624f59a7f6be940359dc5e557257f74f2c9bc0d
thomasung <thomas.ung@pal-robotics.com>
2025-01-15 12:04:24 +0100

    remove install script

--- install.bash
+++
@@ -1,7 +0,0 @@
-# ask if you want to add p10k file
-
-ln -s $(pwd)/p10k/.p10k.zsh ~/.p10k.zsh
-ln -s $(pwd)/nvim ~/.config/nvim
-ln -s $(pwd)/tmux/tmux.conf ~/.config/tmux/tmux.conf
-ln -s $(pwd)/sketchybar/sketchybarrc ~/.config/sketchybar/sketchybarrc
-ln -s $(pwd)/tmux/plugins/tmux/custom ~/.config/tmux/plugins/custom
