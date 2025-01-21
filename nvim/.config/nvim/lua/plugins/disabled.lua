2423b69079fee0cd88190b97e4577b2a562385ca
thomasung <thomas.ung@pal-robotics.com>
2025-01-21 16:51:15 +0100

    disable neotree

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   -- { "folke/trouble.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = true },
   { "garymjr/nvim-snippets", enabled = true },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
