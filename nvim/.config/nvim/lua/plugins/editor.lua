0ea0c073e3cdd7cd91bf86f04dfb2555beb2971c
thomasung <thomas.ung@pal-robotics.com>
2025-01-23 13:11:11 +0100

    add keymap for oil and add some custom nvim dap config

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -76,6 +76,57 @@ return {
   {
     "stevearc/oil.nvim",
     ---@module 'oil'
+    opts = {},
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    opts = function()
+      local dap = require("dap")
+      if not dap.adapters["codelldb"] then
+        require("dap").adapters["codelldb"] = {
+          type = "server",
+          host = "localhost",
+          port = "${port}",
+          executable = {
+            command = "codelldb",
+            args = {
+              "--port",
+              "${port}",
+            },
+          },
+        }
+      end
+      for _, lang in ipairs({ "c", "cpp" }) do
+        dap.configurations[lang] = {
+          {
+            type = "codelldb",
+            request = "launch",
+            name = "Launch file",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "codelldb",
+            request = "launch",
+            name = "Launch robot_joint_limit_estimator",
+            program = "/home/user/exchange/validation_ros2_ws/install/robot_joint_limit_estimator/lib/robot_joint_limit_estimator/joint_limit_estimator_node",
+            args = {
+              "--ros-args --log-level DEBUG --ros-args -r __node:=joint_limit_estimator --params-file /home/user/exchange/validation_ros2_ws/install/robot_joint_limit_estimator/share/robot_joint_limit_estimator/config/tiago_motions.yaml --params-file /home/user/exchange/validation_ros2_ws/install/robot_joint_limit_estimator/share/robot_joint_limit_estimator/config/tiago_configuration.yaml --params-file /tmp/launch_params_nix_ez3t",
+            },
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "codelldb",
+            request = "attach",
+            name = "Attach to process",
+            pid = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+    end,
   },
 }
