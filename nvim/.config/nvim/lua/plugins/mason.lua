f17e83c0a1522e6fb017612cf22dc30976f478c3
thomasung <thomas.ung@pal-robotics.com>
2025-04-23 10:03:16 +0200

    add template to add custom lsp path

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,10 +1,20 @@
 return {
-  -- Ensure C/C++ debugger is installed
-  "williamboman/mason.nvim",
-  optional = true,
-  opts = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "codelldb" })
-    end
-  end,
+  {
+    -- Ensure C/C++ debugger is installed
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "codelldb" })
+      end
+    end,
+  },
+  {
+    require("mason-lspconfig").setup_handlers({
+      -- Default handler
+      function(server_name)
+        require("lspconfig")[server_name].setup({})
+      end,
+    }),
+  },
 }
