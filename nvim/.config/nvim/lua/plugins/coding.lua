ee664fed483b788c5c745453e6b7114d84ac6710
thomasung <thomas.ung@pal-robotics.com>
2025-04-23 10:01:52 +0200

    fix some pilot update abi breakage

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -88,4 +88,11 @@ return {
     },
   },
   { "ThePrimeagen/vim-be-good" },
+  {
+    "zbirenbaum/copilot.lua",
+    optional = true,
+    opts = function()
+      require("copilot.api").status = require("copilot.status")
+    end,
+  },
 }
