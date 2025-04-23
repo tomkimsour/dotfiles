8399add2ec511b1ff3b4ae6b730f7705d7d0d6aa
thomasung <thomas.ung@pal-robotics.com>
2025-04-23 09:55:34 +0200

    home manager update

---
+++ home-manager/.config/home-manager/flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Home Manager Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixGL = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-stable,
+    home-manager,
+    nixGL,
+    nix-index-database,
+    ...
+  }: let
+      system = "x86_64-linux";
+      pkgsConfig = {
+        allowUnfree = true;
+        allowUnfreePredicate = _: true;
+      };
+      pkgs = import nixpkgs {
+        inherit system;
+        config = pkgsConfig;
+      };
+      pkgs-stable = import nixpkgs-stable {
+        inherit system;
+        config = pkgsConfig;
+      };
+    in {
+      homeConfigurations."thomasung" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit pkgs-stable;
+          inherit nixGL;
+        };
+        # Useful stuff for managing modules between hosts
+        # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/modularize-the-configuration
+        modules = [
+          nix-index-database.hmModules.nix-index
+          ./modules/home.nix
+          ./modules/gui
+        ];
+      };
+  };
+}
---
+++ home-manager/.config/home-manager/modules/gui/default.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Fix for GPU stuff on non-nixos systems
+    ./nixgl.nix
+    # ./hyprland.nix
+  ];
+
+  # Add packages
+  # home.packages = lib.mkMerge [
+  #   (with pkgs; [
+  #     grim
+  #     slurp
+  #     swaybg
+  #     wdisplays
+  #   ])
+  # ];
+}
