b892d2e658a13e2f88667fff2b9d787f22c95259
thomasung <thomas.ung@pal-robotics.com>
2025-08-18 16:44:47 +0200

    update home manager

--- home-manager/.config/home-manager/modules/home.nix
+++ home-manager/.config/home-manager/modules/home.nix
@@ -20,7 +20,7 @@
     # You should not change this value, even if you update Home Manager. If you do
     # want to update the value, then make sure to first check the Home Manager
     # release notes.
-    stateVersion = "24.11"; # Please read the comment before changing.
+    stateVersion = "25.05"; # Please read the comment before changing.
 
     # The home.packages option allows you to install Nix packages into your
     # environment.
@@ -37,6 +37,10 @@
       eza
       anki
       valgrind
+      tlrc
+      lolcat
+      timer
+      rustscan # rust nmap
       # # Adds the 'hello' command to your environment. It prints a friendly
       # # "Hello, world!" when run.
       # pkgs.hello
