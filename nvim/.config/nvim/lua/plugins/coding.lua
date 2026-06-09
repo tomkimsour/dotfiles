6828b6766fdddb2b7dd808a38ee0b07da50f29ac
thomasung <thomas.ung@pal-robotics.com>
2026-06-09 12:31:22 +0200

    remove copilot

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,9 +1,1 @@
-return {
-  {
-    "zbirenbaum/copilot.lua",
-    optional = true,
-    opts = function()
-      require("copilot.api").status = require("copilot.status")
-    end,
-  },
-}
+return {}
